b01100ee566927e6b6f3a953c87ab6df33456366
mouli-011 <mouli.bv@zohocorp.com>
2023-04-12 13:00:43 +0530

    Edit Text Disabled

--- app/src/main/java/com/example/calculator/OperationFragment.kt
+++ app/src/main/java/com/example/calculator/OperationFragment.kt
@@ -244,5 +244,8 @@ class OperationFragment : Fragment() {
     private fun EditText.makeInaccessible() {
         isClickable = false
         isFocusable = false
+        setOnClickListener {
+            Toast.makeText(context, R.string.no_operation_chosen_error, Toast.LENGTH_SHORT).show()
+        }
     }
 }
